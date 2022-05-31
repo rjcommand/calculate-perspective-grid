dab1a427cead9d9413d548a2b6ee45b9327ade2d
Rylan Command <rylancommand95@gmail.com>
2022-05-31 11:11:04 -0230

    Crop borders and add focal point

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import perspective_grid
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    img = perspective_grid.load_image('image.jpg')
-    perspective_grid.draw_grid(img=img)
+    img = perspective_grid.load_image('test_image.jpg')
+    cropped = perspective_grid.remove_borders(img)
+    perspective_grid.draw_grid(img=cropped)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
