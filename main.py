02374f61cd5661fb6f4c954f407c16f728947fac
Rylan Command <rylancommand95@gmail.com>
2022-05-31 14:48:54 -0230

    Clean up header text

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-# This is a sample Python script.
+# This is a script to calculate a Canadian (perspective) grid for seafloor images.
+# Following Wakefield & Genin 1987 (https://doi.org/10.1016/0198-0149(87)90148-8)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Press ⌃R to execute
+
+# Import module containing functions to create the grid
 import perspective_grid
 
 # Press the green button in the gutter to run the script.
@@ -9,4 +11,4 @@ if __name__ == '__main__':
     img = perspective_grid.load_image('test_image.jpg')
     cropped = perspective_grid.remove_borders(img)
     perspective_grid.draw_grid(img=cropped)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
