dc2264beda298878a1901dd57ca6df3b54d45798
Rylan Command <rylancommand95@gmail.com>
2022-05-31 14:48:42 -0230

    Add vline and hline calculations

--- perspective_grid.py
+++ perspective_grid.py
@@ -1,6 +1,11 @@
+import math
 import cv2
 import numpy as np
 
+
+# - - - - - - - - - - - - - - - - - #
+# Image loading and pre-processing  #
+# - - - - - - - - - - - - - - - - - #
 
 # Load image
 def load_image(path_to_image):
@@ -24,8 +29,13 @@ def remove_borders(img):
     return crop
 
 
+# - - - - - - - - - - - - - - - - - - - #
+# Calculate & display perspective grid  #
+# - - - - - - - - - - - - - - - - - - - #
+
 # Find point P (the focal point in the image)
 # This assumes that the camera is calibrated so that diagonal lines drawn from corner to corner intersect at P
+# Following: https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines
 def line_intersection(line1, line2):
     xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
     ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
@@ -44,6 +54,66 @@ def line_intersection(line1, line2):
 
 
 # Draw the perspective grid
+# Definitions of points and angles from Wakefield & Genin (1987)
+# O     = nodal point of camera's lens
+# N     = nadir point, where plumb line from nodal point intersects substratum
+# ON    = camera's height above the seafloor
+# p     = principal point, intersection of optical axis with image
+# theta = inclination of optical axis (below the horizontal line): angle(POR)
+# alpha = vertical acceptance angle / angle of view: angle(BOU)
+# beta  = horizontal acceptance angle / angle of view: angle(FOE)
+# base line = the lower edge of the image, ef
+# horizontal lines = Canadian grid lines parallel to the base line (ef)
+# meridian lines = Canadian grid lines, all converging on a single "vanishing point"
+# principal line = Canadian grid line (bu) which intersects the principal point (p) and is perpendicular to ef
+
+# Calculate horizontal lines (vertical scale)
+# Angles
+theta = 22.2
+alpha = 47.4
+beta = 60.4
+# Distances
+ON = 44.11
+BA = 20  # This is the grid interval we want
+# Distance from the base line to the principal point
+bp = (ON * math.tan(90 - (alpha / 2) - theta)) - (ON * math.tan(90 - theta))
+
+# Distance to the first horizontal line in the image, representing a distance of 20cm from baseline on the seafloor
+ba = bp * (1 + (math.tan(theta - math.atan(ON / (BA + ON * (1 / math.tan(theta + alpha / 2)))))) / math.tan(alpha / 2))
+
+var = (10, 20, 30, 40, 50)
+
+
+def calc_h_line(img, theta=22.2, alpha=47.4, camera_height=44.11, grid_interval=100):
+    # Convert degree angle to radians
+    theta = theta * (math.pi / 180)  # Angle of incidence with the seafloor (input as degrees)
+    alpha = alpha * (math.pi / 180)  # Vertical acceptance angle (input as degrees)
+    ON = camera_height  # This is the height of the camera (cm)
+    BA = grid_interval  # This is the grid interval we want
+    # Distance from the base line to the principal point
+    bp = ((ON * math.tan((90 * math.pi/180) - theta)) - (ON * math.tan((90 * math.pi/180) - (alpha / 2) - theta)))
+
+    # Distance to the first horizontal line in the image, representing a distance of 20cm from baseline on the seafloor
+    ba = bp * (1 + (math.tan(theta - math.atan(ON / (BA + ON * (math.cos(theta + alpha / 2) / math.sin(theta + alpha / 2)))))) /
+               math.tan(alpha / 2))
+
+    # Return the points with respect to the image frame
+    # return ba
+    return [(0, int(img.shape[0] - ba)), (int(img.shape[1]), int(img.shape[0] - ba))]
+
+
+# Calculate vertical lines (horizontal scale)
+def calc_v_line(ON, theta, alpha, beta):
+    OB = ON / (math.sin(theta + (alpha / 2)))
+
+    EF = 2 * OB * math.tan(beta / 2)
+
+    Shb = (EF * math.sin(theta + alpha / 2)) / (2 * ON * math.tan(beta / 2))
+
+    return EF, Shb
+
+
+# Draw the perspective grid ontop of the image
 def draw_grid(img, color=(0, 255, 0)):
     # Add intersecting lines to find the focal point
     # Line sf
@@ -59,7 +129,7 @@ def draw_grid(img, color=(0, 255, 0)):
 
     # Find the principal point (where the two diagonals intersect)
     principal_point = line_intersection(line1=((0, 0), (int(img.shape[1]), int(img.shape[0]))),
-                                    line2=((0, int(img.shape[0])), (int(img.shape[1]), 0)))
+                                        line2=((0, int(img.shape[0])), (int(img.shape[1]), 0)))
     # Draw the focal point
     cv2.circle(img, center=(int(principal_point[0]), int(principal_point[1])),
                radius=4, color=(0, 0, 255), thickness=5)
@@ -78,7 +148,10 @@ def draw_grid(img, color=(0, 255, 0)):
              pt2=(int(principal_point[0]), int(img.shape[0])),
              color=color, thickness=2)
     # Distances between reciprocal edges and p are equal (gp = ph; bp = pu)
-
+    # Horizontal lines
+    hlines = np.array([calc_h_line(img, theta=22.2, alpha=47.4, camera_height=44.11, grid_interval=x)
+                       for x in (600, 800)])
+    cv2.polylines(img, hlines, isClosed=True, color=(255, 255, 255), thickness=1)
 
     cv2.imshow('grid', img)
     cv2.waitKey(0)
