00e14d388b0cad823a605cbae1be79e54b24f571
Rylan Command <rylancommand95@gmail.com>
2022-06-01 11:27:12 -0230

    Add vertical line calculation
    * Created a function to calculate vertical lines
    * Re-arranged code to be more modular
    * Created a function to draw the guide lines
    * Created a function to draw the grid lines
    * Created a function to overlay the grid and guides

--- perspective_grid.py
+++ perspective_grid.py
@@ -81,8 +81,6 @@ bp = (ON * math.tan(90 - (alpha / 2) - theta)) - (ON * math.tan(90 - theta))
 # Distance to the first horizontal line in the image, representing a distance of 20cm from baseline on the seafloor
 ba = bp * (1 + (math.tan(theta - math.atan(ON / (BA + ON * (1 / math.tan(theta + alpha / 2)))))) / math.tan(alpha / 2))
 
-var = (10, 20, 30, 40, 50)
-
 
 def calc_h_line(img, theta=22.2, alpha=47.4, camera_height=44.11, grid_interval=20):
     # Convert degree angle to radians
@@ -97,49 +95,91 @@ def calc_h_line(img, theta=22.2, alpha=47.4, camera_height=44.11, grid_interval=
     ba = bp * (1 + (math.tan(theta - math.atan(ON / (BA + ON * (math.cos(theta + alpha / 2) / math.sin(theta + alpha / 2)))))) /
                math.tan(alpha / 2))
 
-    # The distance
-    (ba - bp) / bp
+    # Find the principal point
     principal_point = line_intersection(line1=((0, 0), (int(img.shape[1]), int(img.shape[0]))),
                                         line2=((0, int(img.shape[0])), (int(img.shape[1]), 0)))
 
+    # Calculate the distance from the horizontal line to the principal point as a ratio
     hline_dist = principal_point[1] * (ba / bp)
+
     # Return the points with respect to the image frame
-    #return ba
     hline_points = [(0, int(img.shape[0] - hline_dist)), (int(img.shape[1]), int(img.shape[0]- hline_dist))]
     return hline_points
 
 
 # Calculate vertical lines (horizontal scale)
-def calc_v_line(ON, theta, alpha, beta):
+def calc_v_line(img, ON=44.11, theta=22.2, alpha=47.4, beta=60.4, grid_side='left', grid_interval=10):
+    theta = theta * (math.pi / 180)
+    alpha = alpha * (math.pi / 180)
+    beta = beta * (math.pi / 180)
+    delta = (90 * math.pi/180) - (alpha / 2) - theta
+
+    bp = ((ON * math.tan((90 * math.pi/180) - theta)) - (ON * math.tan((90 * math.pi/180) - (alpha / 2) - theta)))
+
+    # Distance from camera to base line, orthogonal to base line
     OB = ON / (math.sin(theta + (alpha / 2)))
 
+    # Width of bottom of field of view
     EF = 2 * OB * math.tan(beta / 2)
+    ef = 2 * math.tan(beta / 2) * (ON / math.cos(delta))
+    # Scaling factor for hb (horizontal scale at the base line
+    Shb = (ef * math.sin(theta + alpha / 2)) / (2 * ON * math.tan(beta / 2))
 
-    Shb = (EF * math.sin(theta + alpha / 2)) / (2 * ON * math.tan(beta / 2))
+    OP = ON / math.sin(theta)
 
-    return EF, Shb
+    GH = 2 * OP * math.tan(beta / 2)
+    # Scaling factor for hp (horizontal scale at the principal point)
+    Shp = (ef * math.sin(theta)) / (2 * ON * math.tan(beta/2))
+
+    # Find the principal point
+    principal_point = line_intersection(line1=((0, 0), (int(img.shape[1]), int(img.shape[0]))),
+                                        line2=((0, int(img.shape[0])), (int(img.shape[1]), 0)))
+    # Distance from left edge of frame to center (principal point along the base line)
+    eb = ef / 2
+    # Distance from the center to the grid interval
+    bk = grid_interval * Shb
+
+    pj = grid_interval * Shp
+    distance_to_vanishing = bk * (bp / (bk - pj))
+
+    # Calculate the distance along the baseline point to line bu (line perpendicular to the center baseline) as a ratio
+    vline_base_point_left = principal_point[0] - principal_point[0] * ((bk / eb) * Shb)
+    vline_base_point_right = principal_point[0] + principal_point[0] * ((bk / eb) * Shb)
+
+    # Calculate the distance from the grid point to the vertical line through the principal point as a ratio
+    vline_p_point_left = principal_point[0] - principal_point[0] * ((pj / eb) * Shp)
+    vline_p_point_right = principal_point[0] + principal_point[0] * ((pj / eb) * Shp)
+
+    # Get the points with respect to the image frame
+    vlines_left = (vline_base_point_left, img.shape[0]), (vline_p_point_left, distance_to_vanishing)
+    vlines_right = (vline_base_point_right, img.shape[0]), (vline_p_point_right, distance_to_vanishing)
+
+    # Return each left and right side
+    if grid_side == 'left':
+        return vlines_left
+    elif grid_side == 'right':
+        return vlines_right
 
 
-# Draw the perspective grid ontop of the image
-def draw_grid(img, color=(0, 255, 0)):
+def draw_guides(img, pt_color=(0, 0, 255), guide_color=(0, 255, 0)):
     # Add intersecting lines to find the focal point
     # Line sf
     cv2.line(img,
              pt1=(0, 0),
              pt2=(int(img.shape[1]), int(img.shape[0])),
-             color=color)
+             color=guide_color)
     # Line et
     cv2.line(img,
              pt1=(0, int(img.shape[0])),
              pt2=(int(img.shape[1]), 0),
-             color=color)
+             color=guide_color)
 
     # Find the principal point (where the two diagonals intersect)
     principal_point = line_intersection(line1=((0, 0), (int(img.shape[1]), int(img.shape[0]))),
                                         line2=((0, int(img.shape[0])), (int(img.shape[1]), 0)))
     # Draw the focal point
     cv2.circle(img, center=(int(principal_point[0]), int(principal_point[1])),
-               radius=4, color=(0, 0, 255), thickness=5)
+               radius=4, color=pt_color, thickness=5)
 
     # Add horizontal and vertical guides through the focal point
     # Line gh
@@ -147,21 +187,61 @@ def draw_grid(img, color=(0, 255, 0)):
     cv2.line(img,
              pt1=(0, int(principal_point[1])),
              pt2=(int(img.shape[1]), int(principal_point[1])),
-             color=color)
+             color=guide_color)
     # Line bu
     # Point 'b' and 'u' are the bottom and top edges of the image, respectively
     cv2.line(img,
              pt1=(int(principal_point[0]), 0),
              pt2=(int(principal_point[0]), int(img.shape[0])),
-             color=color, thickness=2)
+             color=guide_color, thickness=2)
+
+    return img
+
+
+def draw_grid(img, color=(255, 255, 255), grid_interval=10):
+    # Find the principal point (where the two diagonals intersect)
+    principal_point = line_intersection(line1=((0, 0), (int(img.shape[1]), int(img.shape[0]))),
+                                        line2=((0, int(img.shape[0])), (int(img.shape[1]), 0)))
+    # Line bu
+    # Point 'b' and 'u' are the bottom and top edges of the image, respectively
+    cv2.line(img,
+             pt1=(int(principal_point[0]), 0),
+             pt2=(int(principal_point[0]), int(img.shape[0])),
+             color=color, thickness=1, lineType=cv2.LINE_AA)
     # Distances between reciprocal edges and p are equal (gp = ph; bp = pu)
     # Horizontal lines
-    intervals = np.arange(10, 150, 10, dtype=object).tolist()
+    h_intervals = np.arange(grid_interval, 160, grid_interval, dtype=object).tolist()
     hlines = np.array([calc_h_line(img, theta=22.2, alpha=47.4, camera_height=44.11, grid_interval=x)
-                       for x in intervals])
+                       for x in h_intervals])
+    # Draw the lines
+    cv2.polylines(img, hlines, isClosed=True, color=color, thickness=1, lineType=cv2.LINE_AA)
 
-    cv2.polylines(img, hlines, isClosed=True, color=(255, 255, 255), thickness=1)
+    # Vertical lines
+    v_intervals = np.arange(grid_interval, 100, grid_interval, dtype=object).tolist()
+    vlines_left = np.array([calc_v_line(img, ON=44.11, theta=22.2, alpha=47.4, beta=60.7, grid_side='left',
+                                        grid_interval=x)
+                            for x in v_intervals])
 
+    vlines_right = np.array([calc_v_line(img, ON=44.11, theta=22.2, alpha=47.4, beta=60.7, grid_side='right',
+                                         grid_interval=x)
+                             for x in v_intervals])
+
+    # Draw the vertical lines
+    cv2.polylines(img, np.int32(vlines_left), isClosed=True, color=color, thickness=1, lineType=cv2.LINE_AA)
+    cv2.polylines(img, np.int32(vlines_right), isClosed=True, color=color, thickness=1, lineType=cv2.LINE_AA)
+
+    return img
+
+
+# Draw the perspective grid ontop of the image
+def overlay_grid(img, guide_color=(0, 255, 0), grid_color=(255, 255, 255), grid_interval=10, show_guides=True):
+    if show_guides:
+        draw_grid(img=img, color=grid_color, grid_interval=grid_interval)
+        draw_guides(img=img, guide_color=guide_color)
+    else:
+        draw_grid(img=img, color=grid_color, grid_interval=grid_interval)
+
+    # Show the image
     cv2.imshow('grid', img)
     cv2.waitKey(0)
-    return img, print(hlines)
+    return img
